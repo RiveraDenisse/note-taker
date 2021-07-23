3e537ef3ef0d7cba3c8ae9c711b521f5641bd6ed
Denisse Rivera <denisse_alejandrar@hotmail.com>
2021-07-22 23:35:38 -0500

    add middleware to accept POST data

--- server.js
+++ server.js
@@ -2,9 +2,13 @@
 const express = require('express');
 //to instantiate the server
 const app = express();
-//use port or if not 3001
+//parse incoming string or array data
+app.use(express.urlencoded({ extended: true}));
+//parse incoming JSON data
+app.use(express.json());
+//use environmental variale if not use 3001 port
 const PORT = process.env.PORT || 3001;
-//to require note-data
+//to require note-data JSON format
 const {notes} = require('./Develop/db/db.json');
 
 
@@ -12,8 +16,16 @@ const {notes} = require('./Develop/db/db.json');
 app.get('/api/notes', (req,res) => {
     res.json(notes);
 });
+
+//for client to add notes
+app.post('/api/notes', (req,res) => {
+    //req.body is where our incoming content will be
+    console.log(req.body);
+    res.json(req.body);
+});
+
 //to make our server to listen 
 //TODO: CHANGE PORT 3001 TO PORT ONCE DEPLOYED TO HEROKU
 app.listen(PORT,() => { 
-    console.log(`API server now on port 3001`);
+    console.log(`API server now on port ${PORT}`);
 });
