7aab00d0fc47a3cc21731695fd9c8e63bc542aa3
Denisse Rivera <denisse_alejandrar@hotmail.com>
2021-07-24 11:44:55 -0500

    get and post working

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+const fs = require('fs');
+const path = require('path');
 //require npm express
 const express = require('express');
 //to instantiate the server
 const app = express();
-//parse incoming string or array data
+//parse incoming string or array data (takes incoming POST data and converts it to value pairings accessed in req.body)
 app.use(express.urlencoded({ extended: true}));
 //parse incoming JSON data
 app.use(express.json());
@@ -12,16 +14,35 @@ const PORT = process.env.PORT || 3001;
 const {notes} = require('./Develop/db/db.json');
 
 
+
+//function to handle new note
+function createNewNote (body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, './Develop/db/db.json'),
+        JSON.stringify({ notes: notesArray }, null, 2)
+    )
+    console.log(body);
+    //return finish code to post route for response
+    return note;
+}
 //to request data from db.json 
 app.get('/api/notes', (req,res) => {
-    res.json(notes);
+    let results = notes;
+    res.json(results);
 });
 
 //for client to add notes
 app.post('/api/notes', (req,res) => {
+    //set id based on what the next index of the array will be
+    req.body.id = notes.length.toString();
+    
+    //add note to json file and note array in this function
+    const note = createNewNote(req.body,notes);
     //req.body is where our incoming content will be
     console.log(req.body);
-    res.json(req.body);
+    res.json(note);
 });
 
 //to make our server to listen 
