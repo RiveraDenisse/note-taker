4b8656d8dededdb0dec8741eaae31963755fe5cc
Denisse Rivera <denisse_alejandrar@hotmail.com>
2021-07-24 16:10:26 -0500

    used npm shortid to generate random ids

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -3,6 +3,8 @@ const router = require('express').Router();
 //to require note-data JSON format
 const {notes} = require('../../db/db.json');
 const createNewNote = require('../../lib/notes');
+//npm package (shortid) to generate ids
+const shortid = require('shortid');
 
 
 //to request data from db.json and return saved notes
@@ -14,7 +16,7 @@ router.get('/notes', (req,res) => {
 //for client to add notes 
 router.post('/notes', (req,res) => {
     //set id based on what the next index of the array will be
-    req.body.id = notes.length.toString();
+    req.body.id = shortid.generate();
     
     //add note to json file and note array in this function
     const note = createNewNote(req.body,notes);
